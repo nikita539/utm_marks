281cc7b928eb1a40f2d4e36e3e7f4fac855549e6
nikita539 <evteev_n@inbox.ru>
2022-10-19 18:17:26 +0300

    created project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const PORT = 3003
+
+
+app.use(express.static(path.join(__dirname,'./dist')))
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, './dist/index.html'))
+});
+
+
+app.listen(PORT, function () {
+    console.log("Application is started at localhost:3003")
+})
