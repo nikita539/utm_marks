c2284cbe7faa1e8e40a727876d7178fbbdb558dd
nikita539 <evteev_n@inbox.ru>
2022-12-14 13:43:10 +0300

    created folders for enums

---
+++ src/enums/paths.js
@@ -0,0 +1,6 @@
+export const PATHS = {
+    MARKET: 'market',
+    SNIPE: 'snipe',
+    SWIFT: 'swift',
+    STORK: 'stork'
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import { checkWord } from './helpers'
-import { URLS } from "./redirectingurls";
+import { URLS } from "./enums/redirectingurls";
+import { PATHS } from "./enums/paths";
 
 document.addEventListener('DOMContentLoaded', function() {
     const path = window.location.href
 
-    if (checkWord(path,"market")) {
+    if (checkWord(path,PATHS.MARKET)) {
         window.location.replace(URLS.MARKET_URL)
         return
     }
 
-    if (checkWord(path,"snipe")) {
+    if (checkWord(path,PATHS.SNIPE)) {
         window.location.replace(URLS.SNIPE_URL)
         return
     }
 
-    if (checkWord(path, "swift")) {
+    if (checkWord(path, PATHS.SWIFT)) {
         window.location.replace(URLS.SWIFT_URL)
         return
     }
 
-    if (checkWord(path,"stork")) {
+    if (checkWord(path,PATHS.STORK)) {
         window.location.replace(URLS.STORK_URL)
         return
     }
